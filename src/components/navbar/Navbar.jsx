9feb98ba4fec97143eef1bd22a30713be74ac194
Cess-stack <celesset@missionreadyhq.com>
2025-05-19 13:24:13 +1200

    finsihed navbar fiile setups

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
+        <nav className="navbar">
+            <div className="navbar-left">
+                <img src="" alt="" />
+                <div className="brand">
+                    <span className="brand-main">Car Identifier</span>
+                    <span className="brand-sub">Identify your car</span>
+                </div>  
+            </div>  
+
+            <ul className="navbar-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Claims</a></li>
+                <li><a href="#">Help & Support</a></li>
+                <li><a href="#">About Us</a></li>
             </ul>
+            
         </nav>
     );
 };
