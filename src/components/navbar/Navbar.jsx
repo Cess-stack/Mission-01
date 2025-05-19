7738d01cfac165a7547c195c0bbef2179168dc3f
Cess-stack <celesset@missionreadyhq.com>
2025-05-19 18:44:09 +1200

    finally made my navbar look heaps cleaner and my navbar.jsx tidied added a login button also

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,14 +3,22 @@ import "./Navbar.css";
 
  function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">🚗 Car Identifier</div>
-      <ul className="nav-links">
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/identify">Identify</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </nav>
+<nav className="navbar">
+  <div className="navbar-left">
+    <img src="logo.png" alt="Brand Logo" className="logo" />
+    <div className="brand">
+      <span className="brand-main">YourBrand</span>
+      <span className="brand-sub">Your tagline</span>
+    </div>
+  </div>
+  <ul className="navbar-links">
+    <li><a href="/">Home</a></li>
+    <li><a href="/identify">Identify</a></li>
+    <li><a href="/about">About</a></li>
+  </ul>
+  <button className="login-button">Login</button>
+</nav>
+
   );
 }
 export default Navbar;  
