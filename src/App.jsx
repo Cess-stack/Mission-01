91dc724636d4cdacd254c020cdb949b2c4639c62
Cess-stack <celesset@missionreadyhq.com>
2025-05-19 18:04:19 +1200

    i have decided to use routes for improved user experience. and am also adding dummy pagesd for my home and about links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import CarIdentifier from './CarIdentifier';
 import Navbar from "./components/navbar/Navbar"; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Identify from './pages/Identify';
+
 
 
 function App() {
   return (
     <>
     <Navbar />
-    <CarIdentifier />
+    <Router>
+      <Routes>
+        <Route path="/" element={<CarIdentifier />} />
+        <Route path="/identify" element={<Identify />} />
+      </Routes>
+    </Router>
     </>
   );
 }
